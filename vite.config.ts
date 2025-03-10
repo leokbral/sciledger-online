97018813190f92c043a1fbcd59947079a580fe2c
leokbral <cabralleonardoferreira@gmail.com>
2025-03-10 16:52:25 -0300

    Novos schemas temporários.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
-	server: {
-		allowedHosts: ['frontend_web'],
-	},
-	preview: {
+    plugins: [sveltekit(), purgeCss()],
+    server: {
+        allowedHosts: ['frontend_web']
+    },
+    preview: {
         allowedHosts: [
             'localhost',
             'scideep.imd.ufrn.br'
         ]
     }
-	
 });
