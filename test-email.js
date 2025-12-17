512a15e294d9e6b08223631013dd551f2efed796
YZhLu <leonardo.ferreira.105@ufrn.edu.br>
2025-12-16 23:13:45 -0300

    chore: update SMTP email configuration in test-email.js and recovery route

--- test-email.js
+++ test-email.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: 'snackonapp@gmail.com',
-        pass: 'foriuquhhkipihfv' // Sua senha de app
+        user: 'sciledger@imd.ufrn.br',
+        pass: 'zfpzbhyzbozzqlvx' // App Password
     },
     debug: true,
     logger: true
@@ -20,7 +20,7 @@ async function testEmail() {
 
         console.log('Enviando email de teste...');
         const info = await transporter.sendMail({
-            from: '"SciLedger Team" <snackonapp@gmail.com>',
+            from: '"SciLedger Team" <sciledger@imd.ufrn.br>',
             to: 'mixim94561@pacfut.com', // Coloque seu email aqui
             subject: 'Teste Nodemailer',
             html: '<h1>Teste funcionando!</h1><p>Email enviado com sucesso.</p>'
